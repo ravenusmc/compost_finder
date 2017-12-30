f0fc6bd09347e91530c18a2d6042c1ab93b1736c
ravenusmc <mcuddy77@gmail.com>
2017-12-30 15:43:54 -0500

    finished sign up page

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,19 @@
+//This file will handle all of the javascript code for the program
+
+//This function will change the background image to dirt 
 function changeBackgroundImage() {
   let target = document.getElementById('signUpMain');
+  let targetTwo = document.getElementById('signupSubHeading');
   target.style.backgroundImage = "url('/static/img/dirt.jpg')";
+  targetTwo.innerHTML = '...Into compost!';
 }
 
+//This function will change the background image back to trash on the 
+//sign up page
 function changeImageBack() {
   let target = document.getElementById('signUpMain');
+  let targetTwo = document.getElementById('signupSubHeading');
+
   target.style.backgroundImage = "url('/static/img/trash.jpg')";
+  targetTwo.innerHTML = '';
 }
