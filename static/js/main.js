0e2feae504639e90f3965f0b347338705f036dd9
ravenusmc <mcuddy77@gmail.com>
2017-12-30 13:45:48 -0500

    working on signup page

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+function changeBackgroundImage() {
+  let target = document.getElementById('signUpMain');
+  target.style.backgroundImage = "url('/static/img/dirt.jpg')";
+}
+
+function changeImageBack() {
+  let target = document.getElementById('signUpMain');
+  target.style.backgroundImage = "url('/static/img/trash.jpg')";
+}
